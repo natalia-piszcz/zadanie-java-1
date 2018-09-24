312fb5de6d698ed51e123228a9f08e448c7c081c
Natalia Piszcz <np93@o2.pl>
2018-09-24 21:46:35 +0200

    javastart

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("Zadanie1");
+        System.out.println("Javastart");
     }
 }
